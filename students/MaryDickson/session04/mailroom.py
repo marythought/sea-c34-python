07dee81a3e7cbca1004a212b586f8c1d7c6442b2
Mary Dickson <mdickson@gmail.com>
2015-03-31 18:17:44 -0700

    updated mailroom.py

--- students/MaryDickson/session04/mailroom.py
+++ students/MaryDickson/session04/mailroom.py
@@ -2,25 +2,38 @@
 
 # add note when safe_input.py completed
 
-d = {"mary": [50], "josh": [50], "cassie": [34]}
+d = {"Mary Dickson": [50, 100], "Joshua Diaz": [50], "Cassie Cat": [34]}
 
-print d
-d["mary"] = 100
-print d
 
-# ok, this method overwrites the existing donation amt. how can I add an amt?
+def createreport(dict):
+    # print donorlist
+    for key, value in dict.items():
+        print "\n", key, value
+        print "number of donations: " + str(len(value))
+        print "sum of donations: $" + str(sum(value))
+        print "average donation size: $" + str(sum(value) / len(value))
 
-d["mary"] = [d["mary"], 50]
-print d
-print d.keys()
-print d["mary"]
-print type(d["mary"])
-print d["mary"][0]
-print type(d["mary"][0])
-print d["mary"][1]
 
-# now I can add an amt & create a tuple that I can then call via place number.
-# let's make this a function
+def add_donation(name, dict):
+    """
+    args: name, dict - name of donor to be added to a given dictionary (dict)
+
+    returns: donation amount
+    """
+    print "You are about to add a donation for %s." % name
+    print ("If this is correct, enter the amount below. "
+           "Otherwise, type MENU to go back.\n")
+    amount = raw_input("What is the amount of the donation? $")
+    while type(amount) != int:
+        try:
+            amount = float(amount)
+            amount = int(amount)
+            return amount
+        except:
+            if amount == "MENU":
+                mainmenu()
+            amount = raw_input("Please enter a valid donation amount. "
+                               "What is the amount of the donation? $")
 
 
 def addamt(name, dict, amt):
@@ -30,99 +43,83 @@ def addamt(name, dict, amt):
         elif name not in dict.keys():
             dict[name] = [amt]
     return dict
-
-addamt("mary", d, 45)
-addamt("OC", d, 1000)
-addamt("josh", d, 56)
-print d
-
-print d["mary"][0]
-print d["mary"][1]
-
-# SO BEAUTIFUL DO NOT TOUCH.
+# SO BEAUTIFUL DO NOT BREAK.
 
 
-"""
+def createletter(name, amount):
+    return ("Dear %s, thank you for your donation" +
+            " of $" + str(amount) + ".\n") % name
+
+
 def sendletter(name, dict):
-    if name in dict:
-        # proceed
-    elif name not in d:
-        # add it
+    """
+
+    """
+    amount = add_donation(name, dict)
+    dict = addamt(name, dict, amount)
+    letter = createletter(name, amount)
+    return letter
+
+
+def nameoptions(name, dict):
+    """
+    given a name, confirms correctness and calls functions to add them to d.
+
+    returns: a dictionary
+    """
+    # name = name.capitalize()
+    if name in dict.keys():
+        proceed = raw_input(str(name) + " is in the donor database."
+                            " Continue [Y]/[N]? :")
+        if proceed.upper() == "Y":
+            print sendletter(name, dict)
+        else:
+            mainmenu()
+    else:
+        proceed = raw_input(str(name) + " is not in the donor database."
+                            " Add? [Y]/[N]? :")
+        if proceed.upper() == "Y":
+            print sendletter(name, dict)
+        else:
+            sendprogram()
+
+
+def listdonors(dict):
+    print dict.keys()
+    sendprogram()
+
+
+def sendprogram():
+    """
+    provides options for adding name to database and returns a name to use
+    in subsequent functions
+    """
+    option = raw_input("Please enter a name, [LIST] for options," +
+                       " or [MENU] to go back :")
+    if option.upper() == "LIST":
+        listdonors(d)
+
+    elif option.upper() == "MENU":
+        mainmenu()
+
+    else:
+        nameoptions(option, d)
+
 
 def mainmenu():
     action = raw_input("Would you like to 'SEND' a thank you note," +
-                       " 'CREATE' a report, or 'QUIT'?")
-    while action.upper() != "QUIT":
-        if action.upper() == "SEND":
-            action = raw_input("Please enter a full name, 'LIST' for options,"+
-                               " or 'MENU' to go back")
-            while action.upper() != "MENU":
-                sendletter(action)
-            else:
-                mainmenu()
-    else:
+                       " 'CREATE' a report, or 'QUIT'? :")
+    if action.upper() == "SEND" or action.upper() == "S":
+        sendprogram()
+        mainmenu()
+    elif action.upper() == "CREATE" or action.upper() == "C":
+        createreport(d)
+        mainmenu()
+    elif action.upper() == "QUIT" or action.upper() == "Q":
         exit()
-
-"""
-
-
-"""
-if action == SEND:
+    else:
+        print "I don't understand that option, please choose again. "
+        mainmenu()
 
 
-
-        if name == "MENU"":
-
-            go back to action prompt
-
-        if name == "LIST"":
-
-            print donorlist.keys #names
-
-            name = raw_input("Please enter a name from the list,
-                or type a new one to add"")
-
-        while name not in donorlist.keys:
-
-            ask if they want to add the name y/n
-
-                if y, add name to donorlist
-
-                if no, ask for name again
-
-        if name in donor list.keys:
-
-            name.value = int(raw_input(ask for donation amount))
-
-            while name.value is not an int,
-
-                ask for name.value again
-
-            add (name,value) to donorlist
-            #how to add multiple donations to one user?
-
-    def createletter(name,value):
-
-        print "Dear %n, thank you for your generous donation of %v.
-        We appreciate your support of our mission, which enables us to provide
-        essential services to hundreds of Seattle families.
-        Sincerely, do great inc."" %n name %v value
-
-    createletter(name,value)
-
-    name = MENU
-
-if action == create:
-
-    def createreport():
-
-        sort donorlist by donation amount
-
-        print donorlist.keys, sum(donorlist.values), count(donorlist.values)
-         and average(donorlist.values)
-
-    # return to main menu
-
-    # action = raw_input("Would you like to SEND a thank you note,
-        CREATE a report, or QUIT?")
-"""
+mainmenu()
