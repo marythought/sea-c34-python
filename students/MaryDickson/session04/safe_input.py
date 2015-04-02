74ccfa4c3bbe227477bfa1877679694196cbbd01
Mary Dickson <mdickson@gmail.com>
2015-04-02 12:57:14 -0700

    updated safe_input file, still not working

--- students/MaryDickson/session04/safe_input.py
+++ students/MaryDickson/session04/safe_input.py
@@ -1,1 +1,12 @@
 # safe_input for task 10
+
+
+def safe_input(function):
+    try:
+        function
+    except EOFError:
+        return None
+    except KeyboardInterrupt:
+        return None
+
+safe_input(name=raw_input("what is your name?"))
