7614fe4cb6c5fc4fdd811d4b11a0751dbe6a5e64
Mary Dickson <mdickson@gmail.com>
2015-04-08 19:38:57 -0700

    adding session07 files

---
+++ students/MaryDickson/session07/circle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""circle class --
+
+fill this in so it will pass all the tests.
+"""
+import math
+
+
+class Circle(object):
+    def __init__(self, radius):
+        self.radius = radius
+        self.diameter = radius * 2
+
+    @property
+    def diameter(self):
+        return self._diameter
+
+
+    @diameter.setter
+    def diameter(self, value):
+        self.radius = value / 2
+        self._diameter = value
+
+
